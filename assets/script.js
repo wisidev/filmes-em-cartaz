ae35beddba9e5c1078ed339b1dc2d63c6f86c585
Willian <willianguis@outlook.com>
2025-03-21 10:10:35 -0300

    Função para exibir filmes no carrossel

--- assets/script.js
+++ assets/script.js
@@ -15,4 +15,18 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => callback(data.results))
             .catch(error => console.error("Erro ao buscar filmes:", error));
     }
+
+    // 3. Função para exibir filmes no carrossel
+    function displayCarousel(movies) {
+        if (carouselContent) {
+            carouselContent.innerHTML = movies.map((movie, index) => `
+                <div class="carousel-item ${index === 0 ? "active" : ""}">
+                    <img src="${IMAGE_BASE_URL}${movie.backdrop_path}" class="d-block w-100" alt="${movie.title}">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>${movie.title}</h5>
+                    </div>
+                </div>
+            `).join("");
+        }
+    }
 });
