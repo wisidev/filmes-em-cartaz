3fc4371d5d08a06f5db471d4cadc870fe3d700a4
Willian <willianguis@outlook.com>
2025-03-21 10:08:45 -0300

    Definindo as variáveis de configuração e elementos

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // 1. Definindo as variáveis de configuração e elementos do DOM
+    const API_KEY = "9bbf7d734588f0a01ba0510c39e7e786";
+    const BASE_URL = "https://api.themoviedb.org/3";
+    const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
+    const carouselContent = document.getElementById("carousel-content");
+    const movieList = document.getElementById("movie-list");
+    const searchBar = document.getElementById("search-bar");
+});
